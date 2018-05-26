rotation issue fixed

models/one_step.py
@@ -29,14 +29,22 @@ class Transition(nn.Module):
         self.uvw_dot_norm = 0
         self.pqr_dot_norm = 0
 
-    def R1(self, phi, v):
-        a1 = phi[:,0]
-        a2 = phi[:,1]
-        a3 = phi[:,2]
-        R1_matrix = self.Tensor([[a2.cos()*a3.cos(), a1.sin()*a2.sin()*a3.cos()-a3.cos()*a3.sin(), a1.cos()*a2.sin()*a3.cos()+a1.sin()*a3.sin()],
-                                [a2.cos()*a3.sin(), a1.sin()*a2.sin()*a3.sin()+a1.cos()*a3.cos(), a1.cos()*a2.sin()*a3.sin()-a1.sin()*a3.cos()],
-                                [-a2.sin(), a1.sin()*a2.cos(), a1.cos()*a2.cos()]])
-        return torch.matmul(R1_matrix, torch.t(v)).view(1,-1)
+    def R1(self, zeta, v):
+        phi = zeta[0,0]
+        theta = zeta[1,0]
+        psi = zeta[2,0]
+
+        R_z = self.Tensor([[psi.cos(), -psi.sin(), 0],
+                            [psi.sin(), psi.cos(), 0],
+                            [0., 0., 1.]])
+        R_y = self.Tensor([[theta.cos(), 0., theta.sin()],
+                            [0., 1., 0.],
+                            [-theta.sin(), 0, theta.cos()]])
+        R_x =  self.Tensor([[1., 0., 0.],
+                            [0., phi.cos(), -phi.sin()],
+                            [0., phi.sin(), phi.cos()]])
+        R = torch.matmul(R_z, torch.matmul(R_y, R_x))
+        return torch.matmul(R, torch.t(v)).view(1,-1)
 
     def R2(self, phi, w):
         a1 = phi[:,0]

simulation/animation.py
@@ -21,7 +21,9 @@ class Visualization:
         self.p3 += np.array([[-self.l, 0.0, 0.0] for x in range(n+1)])
         self.p4 += np.array([[0.0, self.l, 0.0] for x in range(n+1)])
 
-    def draw3d(self, ax, xyz, R):
+    def draw3d(self, ax):
+        xyz, R = self.aircraft.xyz, self.aircraft.R1(self.aircraft.zeta).T
+        
         ax.scatter(xyz[0,0], xyz[1,0], xyz[2,0], color='black')
         ax.quiver(xyz[0,0], xyz[1,0], xyz[2,0], R[0,0], R[0,1], R[0,2], pivot='tail', color='red')
         ax.quiver(xyz[0,0], xyz[1,0], xyz[2,0], R[1,0], R[1,1], R[1,2], pivot='tail', color='green')

simulation/quadrotor.py
@@ -57,6 +57,7 @@ class Quadrotor:
                             [0.],
                             [-9.81]])
         self.rpm = np.array([0.0, 0., 0., 0.])
+        
 
     def set_state(self, xyz, zeta, uvw, pqr):
         """
@@ -104,18 +105,16 @@ class Quadrotor:
         phi = zeta[0,0]
         theta = zeta[1,0]
         psi = zeta[2,0]
-        x11 = cos(theta)*cos(psi)
-        x12 = -cos(theta)*sin(psi)+sin(phi)*sin(theta)*cos(psi)
-        x13 = sin(phi)*sin(psi)+cos(phi)*sin(theta)*cos(psi)
-        x21 = cos(theta)*sin(psi)
-        x22 = cos(phi)*cos(psi)+sin(phi)*sin(theta)*sin(psi)
-        x23 = -sin(phi)*cos(psi)+cos(phi)*sin(theta)*sin(psi)
-        x31 = -sin(theta)
-        x32 = sin(phi)*cos(theta)
-        x33 = cos(phi)*cos(theta)
-        return np.array([[x11, x12, x13],
-                        [x21, x22, x23],
-                        [x31, x32, x33]])
+        R_z = np.array([[cos(psi), -sin(psi), 0],
+                            [sin(psi), cos(psi), 0],
+                            [0., 0., 1.]])
+        R_y = np.array([[cos(theta), 0., sin(theta)],
+                            [0., 1., 0.],
+                            [-sin(theta), 0, cos(theta)]])
+        R_x =  np.array([[1., 0., 0.],
+                            [0., cos(phi), -sin(phi)],
+                            [0., sin(phi), cos(phi)]])
+        return R_z.dot(R_y.dot(R_x))
 
     def R2(self, zeta):
         """
@@ -136,6 +135,7 @@ class Quadrotor:
         return np.array([[x11, x12, x13],
                         [x21, x22, x23],
                         [x31, x32, x33]])
+
     
     def aero_forces(self):
         """

train_one_step_model.py
@@ -12,7 +12,7 @@ style.use("seaborn-deep")
 
 def main():
 
-    epochs = 250000
+    epochs = 25000
     state_dim = 12
     action_dim = 4
     hidden_dim = 256
@@ -69,7 +69,7 @@ def main():
 
         # generate random action, assume hover at 50%
         action = np.random.uniform(low=0, high=500, size=(4,))
-        _, _, _, _, _, _, uvw_dot, pqr_dot = iris.step(action)
+        _, _, _, _, _, _, uvw_dot, pqr_dot = iris.step(action, return_acceleration=True)
 
         # update network
         v_dot_loss, w_dot_loss = dyn.update(zeta_rand, uvw_rand, pqr_rand, action, uvw_dot, pqr_dot)
@@ -115,7 +115,7 @@ def main():
             fig1.savefig('vdot_loss.pdf', bbox_inches='tight')
             fig2.savefig('wdot_loss.pdf', bbox_inches='tight')
             print("Saving model")
-            torch.save(dyn, "/models/one_step.pth.tar")
+            torch.save(dyn, "/home/seanny/quadrotor/models/one_step.pth.tar")
 
         counter += 1
 

validate_sim.py
@@ -28,6 +28,7 @@ def main():
     counter = 0
     frames = 100
     rpm = trim+50
+
     for t in time:
 
         iris.step(rpm)
@@ -37,7 +38,7 @@ def main():
 
             pl.figure(0)
             axis3d.cla()
-            vis.draw3d(axis3d, iris.xyz, iris.R1(iris.zeta).T)
+            vis.draw3d(axis3d)
             axis3d.set_xlim(-3, 3)
             axis3d.set_ylim(-3, 3)
             axis3d.set_zlim(0, 6)
@@ -47,8 +48,7 @@ def main():
             axis3d.set_title("Time %.3f s" %t)
             pl.pause(0.001)
             pl.draw()
-        input("Press any key")
-        rpm += np.array([0., 0., 0.5, 0.])
+        rpm += np.array([0.5, 0., 0., 0.])
 
 if __name__ == "__main__":
     main()